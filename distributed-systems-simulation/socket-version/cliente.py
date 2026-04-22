93d8b1381c04f07be1ca4415d3e74f78d881eb4b
Yan Macedo <134351027+Yanmaiscedo@users.noreply.github.com>
2026-04-21 23:15:11 -0300

    Create cliente.py

---
+++ distributed-systems-simulation/socket-version/cliente.py
@@ -0,0 +1,32 @@
+import socket
+import time
+import random
+from config import HOST, PORT
+
+
+def cliente(id):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+
+        print(f"👤 Cliente {id} entrou na barbearia")
+
+        resposta = s.recv(1024).decode()
+
+        if resposta.strip() == "CHEIA":
+            print(f"❌ Cliente {id} foi embora (lotado)")
+        else:
+            print(f"💇 Cliente {id} foi atendido")
+
+        s.close()
+
+    except Exception as e:
+        print(f"Erro cliente {id}: {e}")
+
+
+if __name__ == "__main__":
+    i = 1
+    while True:
+        cliente(i)
+        i += 1
+        time.sleep(random.uniform(0.5, 2))
